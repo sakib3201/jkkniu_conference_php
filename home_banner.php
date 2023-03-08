da7598433a3cbb1bb29a19a1a905a5ac754c9bb9
Mehedi Khan Rakib <mkrakib007@gmail.com>
2023-03-08 23:34:43 +0600

    update in home banner

--- home_banner.php
+++ home_banner.php
@@ -1,5 +1,5 @@
 <div class="card text-bg-dark">
-    <img src="Images/conference.jpg" class="card-img" alt="bg_img" style="height: 100vh;opacity:0.25; position: relative">
+    <img src="Images/conference.jpg" class="card-img" alt="bg_img" style="height: 90vh;opacity:0.25; position: relative">
     <div class="card-img-overlay text-center home-banner" style="position: absolute; top: 10%">
         <h1 class="card-title display-6 fw-bold text-uppercase">International conference on <br> <span class="text-warning">Technology, Business and Justice towards smart Bangladesh</span></h1>
         <p class="card-text fs-2 fw-bold">ICTBJ-2023</p>
