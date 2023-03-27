3eb62d78267f4b59965a690cfb6d95eed477ef5b
rakib8799 <mkrakib007@gmail.com>
2023-03-27 21:45:09 +0600

    again

--- news_scroller.php
+++ news_scroller.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <marquee direction="left" height="30px" style="font-size: 1.2em;" width="100%">
     <!-- <p class="text-bold p-0"> * All presented paper will be included in ICTBJ-2023 proceedings and <span class="fw-bolder">digitally published by</span> <span class="fw-bolder" style="color:darkred;"> Unipress Australia</span> <span class="fw-bolder"> * "Best paper award" </span> will be given to student author and professional author for each track. * </p> -->
     <?php
@@ -13,8 +12,4 @@
     <?php
     }
     ?>
-=======
-<marquee direction="left" height="30px" style="font-size: 1.2em;" width="100%">
-    <p class="text-bold p-0"> * All presented paper will be included in ICTBJ-2023 proceedings and <span class="fw-bolder">digitally published by</span> <span class="fw-bolder" style="color:darkred;"> Unipress Australia</span> <span class="fw-bolder"> * "Best paper award" </span> will be given to student author and professional author for each track. * </p>
->>>>>>> 10b902758a92b82f89998d43ae8853ff8bd5f53d
 </marquee>
